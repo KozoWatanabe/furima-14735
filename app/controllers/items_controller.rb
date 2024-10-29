8b955c5c31b27a5b177d4fdf2b82823906412cf5
KozoWatanabe <f355watanabe@gmail.com>
2024-10-29 11:57:59 +0900

    authenticate_user!をitems_controllerに移動

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  # before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
   end
