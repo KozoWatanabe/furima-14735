660231e6980617ab212fd29073696564227dca2b
KozoWatanabe <f355watanabe@gmail.com>
2024-10-31 21:50:02 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,3 +8,12 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 end
+
+private
+
+def item_params
+  params.require(:item).permit(
+    :name, :description, :category_id, :condition_id, :shipping_cost_id,
+    :prefecture_id, :shipping_time_id, :price, :image
+  ).merge(user_id: current_user.id)
+end
