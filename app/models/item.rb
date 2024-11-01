32fbc68c0e707f778f0135897ad5854def652548
KozoWatanabe <f355watanabe@gmail.com>
2024-11-01 14:48:26 +0900

    商品出品ページを表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping_cost
-  belongs_to :prefecture
-  belongs_to :shipping_time
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_time
 
   validates :name, presence: true
   validates :description, presence: true
--- db/migrate/20241101001619_add_references_to_items.rb
+++
@@ -1,9 +0,0 @@
-class AddReferencesToItems < ActiveRecord::Migration[7.0]
-  def change
-    add_column :items, :category_id, :integer
-    add_column :items, :condition_id, :integer
-    add_column :items, :shipping_cost_id, :integer
-    add_column :items, :prefecture_id, :integer
-    add_column :items, :shipping_time_id, :integer
-  end
-end
