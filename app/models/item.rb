f6adfe7442d0a9958b171e71d283f1775f578d09
KozoWatanabe <f355watanabe@gmail.com>
2024-11-02 07:55:50 +0900

    item モデル　単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-
+  validates_format_of :price, with: /\A[0-9]+\z/, message: 'は半角数値で入力してください'
   validates :image, presence: true
   validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_time_id,
             numericality: { other_than: 0, message: 'を選択してください' }
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,22 @@
 FactoryBot.define do
   factory :item do
-    
+    name { 'Sample Item' }
+    description { 'This is a sample description for the item.' }
+    price { 500 }
+    category_id { 1 }          # 有効なカテゴリーIDを指定
+    condition_id { 1 }         # 有効な状態IDを指定
+    shipping_cost_id { 1 }     # 有効な配送料負担IDを指定
+    prefecture_id { 1 }        # 有効な都道府県IDを指定
+    shipping_time_id { 1 }     # 有効な発送日数IDを指定
+    association :user          # `user`ファクトリを用いた関連付け
+
+    # テスト時の画像添付
+    after(:build) do |item|
+      item.image.attach(
+        io: File.open(Rails.root.join('spec/fixtures/files/sample_image.jpg')),
+        filename: 'sample_image.jpg',
+        content_type: 'image/jpeg'
+      )
+    end
   end
 end
