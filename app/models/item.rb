1edc69b5cb56d295e47a6fc2e52a4f1e61fcd23e
KozoWatanabe <f355watanabe@gmail.com>
2024-11-01 10:27:20 +0900

    itemsモデルにバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,13 @@ class Item < ApplicationRecord
   belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_time
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  validates :image, presence: true
+  validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_time_id,
+            numericality: { other_than: 0, message: 'を選択してください' }
 end
