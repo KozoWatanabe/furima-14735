ee63c50de0548b8469e9d06b57f0c657f24225ea
KozoWatanabe <f355watanabe@gmail.com>
2024-11-14 15:44:10 +0900

    購入画面への遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,8 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    # 商品情報を取得（例: アイテム詳細表示時に渡す）
+    @item = Item.find(params[:item_id])
   end
 end
