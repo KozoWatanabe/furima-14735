c09de35e5b44d7709b00e0c8178b640092337d56
KozoWatanabe <f355watanabe@gmail.com>
2024-11-15 21:41:13 +0900

    OrderSharedAddressクラスのテストコード

---
+++ spec/factories/order_shared_addresses.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_shared_address do
+    postal_code       { '123-4567' }
+    prefecture_id     { Faker::Number.between(from: 1, to: 47) }
+    city              { Faker::Address.city }
+    address           { Faker::Address.street_address }
+    building_name     { Faker::Address.secondary_address }
+    phone_number      { Faker::Number.leading_zero_number(digits: 11) }
+    token             { 'tok_abcdefghijk00000000000000000' }
+    user_id           { 1 }
+    item_id           { 1 }
+  end
+end
---
+++ spec/models/order_shared_address_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+RSpec.describe OrderSharedAddress, type: :model do
+  describe '注文情報の保存' do
+    before do
+      @order_shared_address = FactoryBot.build(:order_shared_address)
+    end
+
+    context '内容に問題がない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_shared_address).to be_valid
+      end
+
+      it 'building_nameは空でも保存できること' do
+        @order_shared_address.building_name = ''
+        expect(@order_shared_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it '郵便番号が必須であること' do
+        @order_shared_address.postal_code = ''
+        @order_shared_address.valid?
+        expect(@order_shared_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+
+      it '郵便番号は「3桁ハイフン4桁」の半角文字列のみ保存可能なこと' do
+        @order_shared_address.postal_code = '1234567'
+        @order_shared_address.valid?
+        expect(@order_shared_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      end
+
+      it '都道府県が必須であること' do
+        @order_shared_address.prefecture_id = 0
+        @order_shared_address.valid?
+        expect(@order_shared_address.errors.full_messages).to include('Prefecture must be selected')
+      end
+
+      it '市区町村が必須であること' do
+        @order_shared_address.city = ''
+        @order_shared_address.valid?
+        expect(@order_shared_address.errors.full_messages).to include("City can't be blank")
+      end
+
+      it '番地が必須であること' do
+        @order_shared_address.address = ''
+        @order_shared_address.valid?
+        expect(@order_shared_address.errors.full_messages).to include("Address can't be blank")
+      end
+
+      it '電話番号が必須であること' do
+        @order_shared_address.phone_number = ''
+        @order_shared_address.valid?
+        expect(@order_shared_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it '電話番号は10桁以上11桁以内の半角数値のみ保存可能なこと' do
+        @order_shared_address.phone_number = '090-1234-5678'
+        @order_shared_address.valid?
+        expect(@order_shared_address.errors.full_messages).to include('Phone number is invalid')
+      end
+
+      it '電話番号は9桁以下では保存できないこと' do
+        @order_shared_address.phone_number = '090123456'
+        @order_shared_address.valid?
+        expect(@order_shared_address.errors.full_messages).to include('Phone number is invalid')
+      end
+
+      it '電話番号は12桁以上では保存できないこと' do
+        @order_shared_address.phone_number = '090123456789'
+        @order_shared_address.valid?
+        expect(@order_shared_address.errors.full_messages).to include('Phone number is invalid')
+      end
+    end
+  end
+end
