87af97f178ff6aee0311dd633a3955f6e77984c2
KozoWatanabe <f355watanabe@gmail.com>
2024-11-24 20:49:36 +0900

    FactoryBotのitem生成を修正
    元の記述ですと@itemでも@order_shared_addressでも同じ@userが使われていることで、テストにおいて出品者と購入者が同一人物になってしまいます。
    @itemはfactorybotのassociationにより自動でユーザーも作られるため、ユーザーの指定が不要です。

--- spec/models/order_shared_address_spec.rb
+++ spec/models/order_shared_address_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe OrderSharedAddress, type: :model do
   describe '注文情報の保存' do
     before do
       @user = FactoryBot.create(:user)
-      @item = FactoryBot.create(:item, user: @user)
+      @item = FactoryBot.create(:item)
       @order_shared_address = FactoryBot.build(:order_shared_address, user_id: @user.id, item_id: @item.id)
     end
 
