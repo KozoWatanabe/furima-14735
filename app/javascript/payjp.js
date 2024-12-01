afd1e551754e3bbf58e5facac892a4976782f0a6
KozoWatanabe <f355watanabe@gmail.com>
2024-12-01 12:00:57 +0900

    カード情報の再マウント

--- app/javascript/payjp.js
+++ app/javascript/payjp.js
@@ -3,13 +3,14 @@ const pay = () => {
   const payjp = Payjp(publicKey);
   const elements = payjp.elements();
 
+  // カード情報の要素を作成
   const cardNumber = elements.create("cardNumber");
-  cardNumber.mount("#number-form");
-
   const cardExpiry = elements.create("cardExpiry");
-  cardExpiry.mount("#expiry-form");
-
   const cardCvc = elements.create("cardCvc");
+
+  // 要素をDOMにマウント
+  cardNumber.mount("#number-form");
+  cardExpiry.mount("#expiry-form");
   cardCvc.mount("#cvc-form");
 
   const form = document.getElementById("payment-form");
@@ -26,20 +27,36 @@ const pay = () => {
           // エラーメッセージを表示
           const errorMessage = document.createElement("span");
           errorMessage.classList.add("error-message");
-          errorMessage.textContent = "Token can't be blank";
+          errorMessage.textContent = result.error.message; // エラー内容を表示
           errorElement.appendChild(errorMessage);
 
-          // 入力欄をクリア
+          // クレジットカード情報をクリア
           cardNumber.clear();
           cardExpiry.clear();
           cardCvc.clear();
+
+          // クレジットカード要素を再度作成・マウント
+          const newCardNumber = elements.create("cardNumber");
+          const newCardExpiry = elements.create("cardExpiry");
+          const newCardCvc = elements.create("cardCvc");
+
+          newCardNumber.mount("#number-form");
+          newCardExpiry.mount("#expiry-form");
+          newCardCvc.mount("#cvc-form");
+
+          // 古い要素を上書き
+          cardNumber = newCardNumber;
+          cardExpiry = newCardExpiry;
+          cardCvc = newCardCvc;
         } else {
+          // トークンをフォームに追加
           const tokenInput = document.createElement("input");
           tokenInput.setAttribute("type", "hidden");
           tokenInput.setAttribute("name", "token");
           tokenInput.setAttribute("value", result.id);
           form.appendChild(tokenInput);
 
+          // クレジットカード情報をクリア
           cardNumber.clear();
           cardExpiry.clear();
           cardCvc.clear();
